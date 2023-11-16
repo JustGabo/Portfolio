a14674b954c6bf148252a00a38be8625e7b81cac
stylessh <stylesshjs@gmail.com>
2023-11-16 18:26:33 -0400

    feat: removing change toggle from navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ModeToggle} from '@/components/modal'
+// import {ModeToggle} from '@/components/modal'
 
 function Navbar() {
   return (
@@ -10,7 +10,7 @@ function Navbar() {
             <li>Projects</li>
             <li>Contact</li>
         </ul> */}
-        <ModeToggle />
+        {/* <ModeToggle /> */}
     </div>
   )
 }
